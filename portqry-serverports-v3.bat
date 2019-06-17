864374c7cf9152b4b2530747f2ea17580a9c048c
benjimmie <43196027+benjimmie@users.noreply.github.com>
2019-06-17 12:41:19 +0800

    v3
    added server:port to txt  & grep + tee to summarise cmd window

---
+++ portqry-serverports-v3.bat
@@ -0,0 +1,12 @@
+@echo off
+ECHO ===============================================
+
+ECHO  Hydra Connectivity Port Tests
+
+ECHO ===============================================
+
+setlocal enabledelayedexpansion
+
+FOR /F "eol=; tokens=1,2 delims=:" %%I IN (serverports.txt) DO (PortQry -n %%I -p TCP -o %%J) | .\gnuwin\grep.exe -e "called" -A 2 -e "TCP" 
+
+pause
